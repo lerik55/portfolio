d337fd21b3f07579def1869926c60e9a8161b78f
Morqqulis <morqqulis@gmail.com>
2025-11-24 17:32:25 +0400

    header menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 Fancybox.bind("[data-fancybox]", {
-    // Your custom options
-});
+   // Your custom options
+})
+
+
+const burger = document.querySelector('.header__burger')
+
+burger.addEventListener('click', function () {
+   document.body.classList.toggle('active')
+})

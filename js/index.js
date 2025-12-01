27759077f399318e402bfddcfea429272f894696
Morqqulis <morqqulis@gmail.com>
2025-12-01 17:20:13 +0400

    mixitup

--- js/index.js
+++ js/index.js
@@ -8,3 +8,6 @@ const burger = document.querySelector('.header__burger')
 burger.addEventListener('click', function () {
    document.body.classList.toggle('active')
 })
+
+
+const mixer = mixitup('.works__galery')
